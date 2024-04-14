81a685f7b5eb264acf9fa00148dcfa549f309e40
artificialair <jaybaker964@gmail.com>
2024-04-14 18:12:29 -0400

    added schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,55 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type BookInput {
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: String  
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(
+      username: String!, 
+      email: String!,
+      password: String!
+    ): Auth 
+    login(
+      email: String!, 
+      password: String!
+    ): Auth 
+    saveBook(
+      bookData: BookInput
+    ): User 
+  removeBook(
+    bookId: String!
+    ): User
+  }
+`;
+
+module.exports = typeDefs;
